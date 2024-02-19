e2f24ea7e7dbf81b9a1343ebf119d8f37bfddd2b
StevanD02 <160432070+StevanD02@users.noreply.github.com>
2024-02-19 12:22:23 +0100

    Update YouTubeController.java

--- src/main/java/com/example/stevan/controller/YouTubeController.java
+++ src/main/java/com/example/stevan/controller/YouTubeController.java
@@ -10,22 +10,8 @@ import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class YouTubeController {
-    private String apiKey = "AIzaSyBxB7aORf1JtuZg2bgMl0rDpSu4VvIBbfA";
-
     private static final String YOUTUBE_SEARCH_URL = "https://www.googleapis.com/youtube/v3/search";
-
     private final RestTemplate restTemplate = new RestTemplate();
-
-    @GetMapping("/youtubeSearch")
-    public String youtubeSearch(@RequestParam("query") String query) {
-        // Construct the URL with the query and API key
-        String searchUrl = YOUTUBE_SEARCH_URL + "?part=snippet&type=video&maxResults=5&q=" + query + "&key=" + apiKey;
-
-        // Make the GET request to YouTube Data API and return the response body
-        ResponseEntity<String> response = restTemplate.getForEntity(searchUrl, String.class);
-
-        return response.getBody();
-    }
 
     @GetMapping("/deezerSearch")
     public ResponseEntity<String> deezerSearch(@RequestParam String query) {
